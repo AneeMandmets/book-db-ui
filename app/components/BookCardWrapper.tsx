8c952b9425cd17043d39f912abf6fb19a3c10bbb
AneeMandmets <anee.mandmets@gmail.com>
2025-03-05 13:54:14 +0200

    Fix?

--- app/components/BookCardWrapper.tsx
+++ app/components/BookCardWrapper.tsx
@@ -3,10 +3,6 @@ import { Book } from "../types";
 import BookCard from "./BookCard";
 
 export default function BookCardWrapper({ book }: { book: Book }) {
-  const handleClick = (book: Book) => {
-    console.log('Clicked book:', book.title);
-    // Handle click event
-  };
 
   return (
     <BookCard 
