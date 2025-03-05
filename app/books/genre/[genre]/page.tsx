8489097dd9ca76a31e58c8563556c461024691a7
AneeMandmets <anee.mandmets@gmail.com>
2025-03-05 15:49:03 +0200

    Fixed paths

---
+++ app/books/genre/[genre]/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { books } from "@/app/data/books";
+import BookCardWrapper from "@/app/components/BookCardWrapper";
+import { useParams } from "next/navigation";
+import { Book } from "@/app/types";
+
+export default function GenrePage() {
+    const params = useParams();
+    const genre = params.genre;
+
+    const filteredBooks = books.filter((book: Book) => book.genre === genre);
+
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold mb-4">Books in {genre}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {filteredBooks.map(book => (
+                    <BookCardWrapper key={book.id} book={book} />
+                ))}
+            </div>
+        </div>
+    );
+        
+}
--- app/ui/SideMenu.tsx
+++ app/ui/SideMenu.tsx
@@ -1,16 +1,25 @@
-const genres = ["Action", "Adventure", "Biography", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Science Fiction", "Thriller"];
+'use client'
+import { useRouter } from "next/navigation";
+
+const genres = ["Action", "Adventure", "Biography", "Comedy", "Classic", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Science Fiction", "Thriller"];
 
 export default function SideMenu() {
+    const router = useRouter();
+
     return (
         <div className="w-1/10">
             <div className="pt-4 flex flex-col ml-8 h-full">
                 <p className="text-2xl font-bold mb-4">Genres</p>
                 {genres.map((genre) => ( 
                     <div key={genre}>
-                        <p className="leading-10">{genre}</p>
+                        <p 
+                            className="leading-10 cursor-pointer hover:text-blue-500" 
+                            onClick={() => router.push(`/genre/${genre}`)}
+                        >
+                            {genre}
+                        </p>
                     </div>
                 ))}
-
             </div>
         </div>
     );
